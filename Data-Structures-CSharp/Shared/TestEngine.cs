4b8e84548018c1b48b015e0c382a0c94d92d59b0
Matt <matt.paauw@gmail.com>
2017-02-17 08:55:00 -0800

    Added comment documentation to the TestEngine class.

--- Data-Structures-CSharp/Shared/TestEngine.cs
+++ Data-Structures-CSharp/Shared/TestEngine.cs
@@ -14,21 +14,33 @@ namespace Data_Structures_CSharp.Shared
 
         private int[] elements;
 
+        /// <summary>
+        /// Default constructor.
+        /// Reads supplied sample size and domain upper bounds, builds an element array and fills it with data.
+        /// </summary>
+        /// <param name="sample"></param>
+        /// <param name="domain"></param>
         public TestEngine(int sample, int domain)
         {
             this.SAMPLE_SIZE_UPPER_BOUND = sample;
             this.DOMAIN_UPPER_BOUND = domain;
-            createArray();
+            buildArray();
             populateArray();
         }
 
-        private void createArray()
+        /// <summary>
+        /// Initializes the element array to a random size based on the provided sample size upper bound.
+        /// </summary>
+        private void buildArray()
         {
             Random rand = new Random();
             int arrSize = rand.Next(0, SAMPLE_SIZE_UPPER_BOUND);
             this.elements = new int[arrSize];
         }
 
+        /// <summary>
+        /// Fills the element array with random positive and negative integers within the absolute value of the provided domain upper bound.
+        /// </summary>
         private void populateArray()
         {
             Random rand = new Random();
