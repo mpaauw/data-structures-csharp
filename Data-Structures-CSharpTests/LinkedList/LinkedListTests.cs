30047e6d675f218a07fdeefb5d302d478e640f39
Matt <matt.paauw@gmail.com>
2017-02-16 19:23:30 -0800

    Added unit test file for LinkedList class.

---
+++ Data-Structures-CSharpTests/LinkedList/LinkedListTests.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Data_Structures_CSharp.LinkedList;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data_Structures_CSharp.LinkedList.Tests
+{
+    [TestClass]
+    public class LinkedListTests
+    {
+        [TestMethod]
+        public void LinkedListTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        public void getSizeTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        public void insertTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        public void deleteTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        public void searchTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
