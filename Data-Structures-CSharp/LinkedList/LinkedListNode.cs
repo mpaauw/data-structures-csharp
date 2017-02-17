f614c8c789dd600318672c0f5c00b9ab2d528fb5
Matt <matt.paauw@gmail.com>
2017-02-16 19:13:53 -0800

    Refactored LinkedNode class to the Node class, designed to represent a more primitive node-based object; refactored the LinkedListNode class to reflect this base class change.

--- Data-Structures-CSharp/LinkedList/LinkedListNode.cs
+++ Data-Structures-CSharp/LinkedList/LinkedListNode.cs
@@ -11,12 +11,37 @@ namespace Data_Structures_CSharp.LinkedList
     /// Class which represents a single node for a Linked List data structure.
     /// </summary>
     /// <typeparam name="T"></typeparam>
-    class LinkedListNode<T> : LinkedNode<T>
+    class LinkedListNode<T> : Node<T>
     {
+        private LinkedListNode<T> next;
+
         /// <summary>
         /// Default constructor.
+        /// Initializes the current node's next pointer to null.
         /// </summary>
         /// <param name="input">Passed to constructor of base class.</param>
-        public LinkedListNode(T input) : base(input) { }
+        public LinkedListNode(T input) : base(input)
+        {
+            this.next = null;
+        }
+
+        /// <summary>
+        /// Returns the current node's next node pointer.
+        /// </summary>
+        /// <returns>Returns current node's next node object.</returns>
+        public LinkedListNode<T> getNext()
+        {
+            return this.next;
+        }
+
+        /// <summary>
+        /// Sets the value of the current node's next pointer to a given object.
+        /// </summary>
+        /// <param name="input">Object to be passed and set as current node's next pointer.</param>
+        public void setNext(LinkedListNode<T> input)
+        {
+            this.next = input;
+        }
     }
+
 }
--- Data-Structures-CSharp/Shared/Node.cs
+++ Data-Structures-CSharp/Shared/Node.cs
@@ -7,24 +7,21 @@ using System.Threading.Tasks;
 namespace Data_Structures_CSharp.Shared
 {
     /// <summary>
-    /// Abstract base class which represents a single node for a linked data structure.
+    /// Abstract base class which represents a single node for a data structure.
     /// </summary>
     /// <typeparam name="T">Underlying data type represented within the class object.</typeparam>
-    abstract class LinkedNode<T>
+    abstract class Node<T>
     {
         private T data;
-
-        private LinkedNode<T> next;
 
         /// <summary>
         /// Default constructor.
         /// Initializes node data to a given input parameter, and initializes node's next pointer to null.
         /// </summary>
         /// <param name="input">Item to be passed and set as current node's data value.</param>
-        public LinkedNode(T input)
+        public Node(T input)
         {
             this.data = input;
-            this.next = null;
         }
 
         /// <summary>
@@ -43,24 +40,6 @@ namespace Data_Structures_CSharp.Shared
         public void setData(T input)
         {
             this.data = input;
-        }
-
-        /// <summary>
-        /// Returns the current node's next node pointer.
-        /// </summary>
-        /// <returns>Returns current node's next node object.</returns>
-        public LinkedNode<T> getNext()
-        {
-            return this.next;
-        }
-
-        /// <summary>
-        /// Sets the value of the current node's next pointer to a given object.
-        /// </summary>
-        /// <param name="input">Object to be passed and set as current node's next pointer.</param>
-        public void setNext(LinkedNode<T> input)
-        {
-            this.next = input;
         }
     }
 }
