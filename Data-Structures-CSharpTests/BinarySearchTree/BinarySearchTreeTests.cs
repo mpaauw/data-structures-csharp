901b9e22d2542d3078b68381914a8d89e80e7b4b
Matt <matt.paauw@gmail.com>
2017-02-17 08:23:20 -0800

    Added initial unit test file for the BinarySearchTree class with boilerplate code.

---
+++ Data-Structures-CSharpTests/BinarySearchTree/BinarySearchTreeTests.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Data_Structures_CSharp.BinarySearchTree;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data_Structures_CSharp.BinarySearchTree.Tests
+{
+    [TestClass]
+    public class BinarySearchTreeTests
+    {
+        [TestMethod]
+        public void BinarySearchTreeTest()
+        {
+        }
+
+        [TestMethod]
+        public void searchTest()
+        {
+        }
+
+        [TestMethod]
+        public void findMinimumTest()
+        {
+        }
+
+        [TestMethod]
+        public void findMaximumTest()
+        {
+        }
+
+        [TestMethod]
+        public void traverseTest()
+        {
+        }
+
+        [TestMethod]
+        public void insertTest()
+        {
+        }
+
+        [TestMethod]
+        public void deleteTest()
+        {
+        }
+
+        [TestMethod]
+        public void balanceTest()
+        {
+        }
+
+        [TestMethod]
+        public void getHeightTest()
+        {
+        }
+    }
+}
