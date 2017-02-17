ad3ebbe577730cfd679c2f1c5aafc77fafb68041
Matt <matt.paauw@gmail.com>
2017-02-16 17:56:21 -0800

    Added LinkedListNode class to LinkedList directory, inherting from LinkedNode base class.

---
+++ Data-Structures-CSharp/LinkedList/LinkedListNode.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Data_Structures_CSharp.Shared;
+
+namespace Data_Structures_CSharp.LinkedList
+{
+    class LinkedListNode<T> : LinkedNode<T>
+    {
+        public LinkedListNode(T input) : base(input) { }
+    }
+}
