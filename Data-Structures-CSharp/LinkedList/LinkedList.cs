c9a6498cdf637c34e7b1f69fd30773de55af1f05
Matt <matt.paauw@gmail.com>
2017-02-16 17:41:56 -0800

    Added directories/code for the Linked List data structure.

---
+++ Data-Structures-CSharp/LinkedList/LinkedList.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data_Structures_CSharp.LinkedList
+{
+    class LinkedList
+    {
+
+    }
+}
