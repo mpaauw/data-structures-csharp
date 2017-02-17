4146a115df3e2d7533d1854a7f568e4e7a0eecac
Matt <matt.paauw@gmail.com>
2017-02-16 19:06:16 -0800

    Added documentation comments to the LinkedListNode class.

--- Data-Structures-CSharp/LinkedList/LinkedListNode.cs
+++ Data-Structures-CSharp/LinkedList/LinkedListNode.cs
@@ -7,8 +7,16 @@ using Data_Structures_CSharp.Shared;
 
 namespace Data_Structures_CSharp.LinkedList
 {
+    /// <summary>
+    /// Class which represents a single node for a Linked List data structure.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
     class LinkedListNode<T> : LinkedNode<T>
     {
+        /// <summary>
+        /// Default constructor.
+        /// </summary>
+        /// <param name="input">Passed to constructor of base class.</param>
         public LinkedListNode(T input) : base(input) { }
     }
 }
