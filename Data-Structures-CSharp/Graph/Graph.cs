55c4ee5c86aab562e6ec4ec792167272367d6145
Matt <matt.paauw@gmail.com>
2017-02-20 17:55:27 -0800

    Refacotred GraphNode class into a GraphEdgeNode class.

---
+++ Data-Structures-CSharp/Graph/Graph.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data_Structures_CSharp.Graph
+{
+    public class Graph<T>
+    {
+        private LinkedList<T> vertices;
+
+        private LinkedList<T> edges;
+
+        private int numVertices;
+
+        private int numEdges;
+
+        public Graph()
+        {
+            this.vertices = new LinkedList<T>();
+            this.edges = new LinkedList<T>();
+            this.numVertices = 0;
+            this.numEdges = 0;
+        }
+
+
+    }
+}
---
+++ Data-Structures-CSharp/Graph/GraphEdgeNode.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Data_Structures_CSharp.Shared;
+
+namespace Data_Structures_CSharp.Graph
+{
+    public class GraphEdgeNode<T> : Node<T>
+    {
+        public GraphEdgeNode(T input, int w = 0) : base(input)
+        {
+            this.weight = w;
+            this.next = null;
+        }
+
+        public GraphEdgeNode<T> next;
+
+        public int weight { get; set; }
+    }
+}
