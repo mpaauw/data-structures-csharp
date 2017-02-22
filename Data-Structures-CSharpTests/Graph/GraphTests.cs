1a3de351563795a0d84ec38ec7852bc695d797d8
Matt <matt.paauw@gmail.com>
2017-02-21 22:10:06 -0800

    Added depthFirstSearchTest() unit test to the GraphTests unit test tile, ran all tests successfully.

--- Data-Structures-CSharpTests/Graph/GraphTests.cs
+++ Data-Structures-CSharpTests/Graph/GraphTests.cs
@@ -15,7 +15,7 @@ namespace Data_Structures_CSharp.Graph.Tests
     public class GraphTests
     {
         private char[] vertices = { 'A', 'B', 'C', 'D', 'E', 'F', 'G' };
-        private int[,] edgeIndices = new int[,] { { 0, 1 }, { 0, 4 }, { 3, 5 }, { 1, 3 }, { 2, 0 }, { 5, 1 }, { 5, 6 } };
+        private int[,] edgeIndices = new int[,] { { 0, 1 }, { 0, 4 }, { 4, 6 }, { 3, 5 }, { 1, 3 }, { 2, 0 }, { 5, 1 }, { 5, 6 } };
         private GraphNode<char> nodes;
         private int numEdges = 6;
         private Graph<char> graph;
@@ -104,7 +104,39 @@ namespace Data_Structures_CSharp.Graph.Tests
             }
             GraphNode<char>[] expectedOrder = { nodes[0], nodes[1], nodes[4], nodes[3], nodes[5] }; 
             GraphNode<char>[] actualOrder = this.graph.breadthFirstSearch();
-            for(int i = 0; i < expectedOrder.Length; i++)
+            for(int i = 0; i < actualOrder.Length; i++)
+            {
+                Assert.AreEqual(expectedOrder[i], actualOrder[i]);
+            }
+        }
+
+        /// <summary>
+        /// Tests if the graph is able to properly perform a Depth First Search on it's underlying data structure.
+        /// </summary>
+        [TestMethod]
+        public void depthFirstSearchTest()
+        {
+            GraphNode<char>[] nodes = new GraphNode<char>[this.vertices.Length];
+            int iter = 0;
+            foreach (char vertex in this.vertices)
+            {
+                GraphNode<char> newNode = new GraphNode<char>(vertex);
+                this.graph.insertVertex(newNode);
+                nodes[iter] = newNode;
+                iter++;
+            }
+            for (int i = 0; i < numEdges; i++)
+            {
+                int[] pair = new int[2];
+                for (int j = 0; j <= 1; j++)
+                {
+                    pair[j] = edgeIndices[i, j];
+                }
+                this.graph.insertEdge(nodes[pair[0]], nodes[pair[1]]);
+            }
+            GraphNode<char>[] expectedOrder = { nodes[0], nodes[4], nodes[6], nodes[1], nodes[3], nodes[5] };
+            GraphNode<char>[] actualOrder = this.graph.depthFirstSearch();
+            for(int i = 0; i < actualOrder.Length; i++)
             {
                 Assert.AreEqual(expectedOrder[i], actualOrder[i]);
             }
