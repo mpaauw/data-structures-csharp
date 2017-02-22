56f4b801327497f1782088d631d41b4bbff7dd0a
Matt <matt.paauw@gmail.com>
2017-02-21 18:15:55 -0800

    Added GraphTests unit test file.

---
+++ Data-Structures-CSharpTests/Graph/GraphTests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Data_Structures_CSharp.Graph;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data_Structures_CSharp.Graph.Tests
+{
+    [TestClass]
+    public class GraphTests
+    {
+        [TestMethod]
+        public void insertVertexTest()
+        {
+        }
+
+        [TestMethod()]
+        public void insertEdgeTest()
+        {
+        }
+    }
+}
