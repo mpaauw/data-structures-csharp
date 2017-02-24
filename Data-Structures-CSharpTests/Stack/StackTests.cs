26386ed6f3660fba337d58f58dc62d2f6ac0d6e7
Matt <matt.paauw@gmail.com>
2017-02-24 10:27:43 -0800

    Added StackTests unit test file.

---
+++ Data-Structures-CSharpTests/Stack/StackTests.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Data_Structures_CSharp.Stack;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data_Structures_CSharp.Stack.Tests
+{
+    [TestClass]
+    public class StackTests
+    {
+        [TestMethod]
+        public void pushTest()
+        {
+        }
+
+        [TestMethod]
+        public void popTest()
+        {
+        }
+
+        [TestMethod]
+        public void isEmptyTest()
+        {
+        }
+    }
+}
