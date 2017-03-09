7d8b8447f59bc7c27c466e7492c72cddec8bd80f
Matt <matt.paauw@gmail.com>
2017-03-08 16:54:53 -0800

    Added QueueTests unit test file.

---
+++ Data-Structures-CSharpTests/Queue/QueueTests.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Data_Structures_CSharp.Queue;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data_Structures_CSharp.Queue.Tests
+{
+    [TestClass]
+    public class QueueTests
+    {
+        public QueueTests()
+        {
+        }
+
+        [TestMethod]
+        public void getSizeTest()
+        {
+        }
+
+        [TestMethod()]
+        public void enqueueTest()
+        {
+        }
+
+        [TestMethod()]
+        public void dequeueTest()
+        {
+        }
+
+        [TestMethod()]
+        public void isEmptyTest()
+        {
+        }
+    }
+}
