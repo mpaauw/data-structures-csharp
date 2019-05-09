4c855940626aed3cd26ec1c4b4321737646a199e
mpaauw <matt.paauw@gmail.com>
2019-05-08 20:27:45 -0700

    Add boilerplate; remove old stuff.

---
+++ src/HashTable/HashTable.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HashTable
+{
+    public class HashTable<TKey, TValue> : IHashTable<TKey, TValue>
+    {
+        private int size;
+        private TValue[] hashTable;
+
+        public HashTable(int size = -1)
+        {
+            this.size = (size == -1) ? new Random().Next(1, 100) : size;
+            this.hashTable = new TValue[this.size];
+        }
+
+        public TValue Get(TKey key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Set(TKey key, TValue value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public TValue Delete(TKey key)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/HashTable/IHashTable.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HashTable
+{
+    public interface IHashTable<TKey, TValue>
+    {
+        TValue Get(TKey key);
+
+        void Set(TKey key, TValue value);
+
+        TValue Delete(TKey key);
+    }
+}
